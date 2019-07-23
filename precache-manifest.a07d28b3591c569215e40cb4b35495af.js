178b998bf4eaa4ad3c92dc9b92bbba9afc943a56
Elmi Rabelo <rabeloelmi@gmail.com>
2019-07-23 15:31:22 -0300

    Updates

---
+++ precache-manifest.a07d28b3591c569215e40cb4b35495af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd97cc38a2475eddd3cd2a2b20d4c37",
+    "url": "/robot-cards/index.html"
+  },
+  {
+    "revision": "00b4b2790195e350a01e",
+    "url": "/robot-cards/static/css/main.d398a6a8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robot-cards/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "00b4b2790195e350a01e",
+    "url": "/robot-cards/static/js/main.466c1f4a.chunk.js"
+  },
+  {
+    "revision": "c454843e232be8059369",
+    "url": "/robot-cards/static/js/runtime~main.a9db020e.js"
+  }
+]);
